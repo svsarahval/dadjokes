f7662fd13b5eb6a0879545d3c895266904b16305
svsarahval <svsarahval@gmail.com>
2023-03-05 22:57:27 -0500

    not working no css

--- src/Components/Jokelist.js
+++ src/Components/Jokelist.js
@@ -3,35 +3,39 @@ import { useFetch } from '../Hooks/useFetch.js';
 
 const Jokelist = () => {
   const [url, setUrl] = useState('GET https://icanhazdadjoke.com/');
-  const { data: jokes } = useFetch(url);
+  const { data: randomJokes } = useFetch(url);
 
   return (
-    <div className='Joke-list'>
-      <h2>Joke List</h2>
-      <ul>
-        {jokes &&
-          jokes.map((joke) => (
-            <li key={trip.id}>
-              <h3> {trip.joke}</h3>
-              <p>{trip.status}</p>
-            </li>
-          ))}
-      </ul>
-      <div className='buttons'>
-        <button onClick={() => setUrl('GET https://icanhazdadjoke.com/search')}>
-          {' '}
-          Search Jokes
-        </button>
-        <button
-          onClick={() => setUrl('GET https://icanhazdadjoke.com/j/<joke_id>')}
-        >
-          {' '}
-          Pick A Joke
-        </button>
-        <button onClick={() => setUrl('GET https://icanhazdadjoke.com/')}>
-          {' '}
-          Random Jokes
-        </button>
+    <div className='listContainer'>
+      <div className='Joke-list'>
+        <h2>Joke List</h2>
+        <ul>
+          {randomJokes &&
+            randomJokes.map((randomJoke) => (
+              <li key={randomJoke.id}>
+                <h3> {randomJoke.joke}</h3>
+                <p>{randomJoke.status}</p>
+              </li>
+            ))}
+        </ul>
+        <div className='buttons'>
+          <button
+            onClick={() => setUrl('GET https://icanhazdadjoke.com/search')}
+          >
+            {' '}
+            Search Jokes
+          </button>
+          <button
+            onClick={() => setUrl('GET https://icanhazdadjoke.com/j/<joke_id>')}
+          >
+            {' '}
+            Pick A Joke
+          </button>
+          <button onClick={() => setUrl('GET https://icanhazdadjoke.com/')}>
+            {' '}
+            Random Jokes
+          </button>
+        </div>
       </div>
     </div>
   );
